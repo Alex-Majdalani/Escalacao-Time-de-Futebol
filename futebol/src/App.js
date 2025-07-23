e6d1e8467b044d197fbdbb6a3c4fdc0f324061c1
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-07-23 14:54:07 -0300

    Criei um estado para os novos jogadores adicionados serem salvos num array

--- futebol/src/App.js
+++ futebol/src/App.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 
 function App() {
+  /*Criar um estado para os jogadores */
+  const [jogadores, setJogadores] = useState([]);
+
+  const aoNovoJogadorAdicionado = (jogador) => {
+    console.log(jogador);
+  };
+
   return (
     <div className="App">
       <h1>Futebol das Quartas</h1>
       <Banner />
-      <Formulario />
+      <Formulario
+        aoJogadorCadastrado={(jogador) => aoNovoJogadorAdicionado(jogador)}
+      />
     </div>
   );
 }

9dcb086b82b6454de3cb0d12fd757bb096d7bd26
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-07-28 15:54:20 -0300

    adicionei um array para os times em app.js, criei um .map para os times com as cores

--- futebol/src/App.js
+++ futebol/src/App.js
@@ -4,6 +4,74 @@ import Formulario from "./componentes/Formulario";
 import Posicoes from "./componentes/Posicoes";
 
 function App() {
+  const posicoes = [
+    {
+      nome: "Goleiro",
+      corPrimaria: "#A8E6A1",
+      corSecundaria: "#F3FDE9",
+    },
+    {
+      nome: "Zagueiro",
+      corPrimaria: "#5EB764",
+      corSecundaria: "#D4F0D3",
+    },
+    {
+      nome: "Lateral Direito",
+      corPrimaria: "#6BB9F0",
+      corSecundaria: "#E3F4FF",
+    },
+    {
+      nome: "Lateral Esquerdo",
+      corPrimaria: "#2980B9",
+      corSecundaria: "#D6ECFF",
+    },
+    {
+      nome: "Volante",
+      corPrimaria: "#FFF176",
+      corSecundaria: "#FFFDE7",
+    },
+    {
+      nome: "Centroavante",
+      corPrimaria: "#FBC02D",
+      corSecundaria: "#FFF9E1",
+    },
+    {
+      nome: "Meia",
+      corPrimaria: "#FFD54F",
+      corSecundaria: "#FFF7DA",
+    },
+    {
+      nome: "Meia Ofensivo",
+      corPrimaria: "#FFCA28",
+      corSecundaria: "#FFF3CD",
+    },
+    {
+      nome: "Meia Defensivo",
+      corPrimaria: "#FFB300",
+      corSecundaria: "#FFF0C2",
+    },
+    {
+      nome: "Ponta Direita",
+      corPrimaria: "#FFB74D",
+      corSecundaria: "#FFF3E0",
+    },
+    {
+      nome: "Ponta Esquerda",
+      corPrimaria: "#F57C00",
+      corSecundaria: "#FFE8D6",
+    },
+    {
+      nome: "Atacante",
+      corPrimaria: "#E53935",
+      corSecundaria: "#FFE5E5",
+    },
+    {
+      nome: "Reserva",
+      corPrimaria: "#212121",
+      corSecundaria: "#BDBDBD",
+    },
+  ];
+
   /*Criar um estado para os jogadores */
   const [jogadores, setJogadores] = useState([]);
 
@@ -19,9 +87,9 @@ function App() {
       <Formulario
         aoJogadorCadastrado={(jogador) => aoNovoJogadorAdicionado(jogador)}
       />
-      <Posicoes nome="Goleiro" />
-      <Posicoes nome="Lateiral Direito" />
-      <Posicoes nome="Zagueiro" />
+      {posicoes.map((posicoes) => (
+        <Posicoes key={posicoes.nome} nome={posicoes.nome} corPrimaria={posicoes.corPrimaria} corSecundaria={posicoes.corSecundaria} />
+      ))}
     </div>
   );
 }
--- futebol/src/componentes/Posicoes/index.js
+++ futebol/src/componentes/Posicoes/index.js
@@ -1,9 +1,12 @@
 import "./Posicoes.css";
 
 const Posicoes = (props) => {
+  const corPrimaria = { borderColor: props.corPrimaria };
+  const corSecundaria = { backgroundColor: props.corSecundaria };
+
   return (
-    <section className="posicao">
-      <h3>{props.nome}</h3>
+    <section className="posicao" style={corSecundaria}>
+      <h3 style={corPrimaria}>{props.nome}</h3>
     </section>
   );
 };

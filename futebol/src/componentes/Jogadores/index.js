7ba7954d6c5b6c5f637b36940aed9e13e4528122
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-08-02 00:15:21 -0300

    Adicionei uma imagem como alternativa caso o usuario não carregue nada

--- futebol/src/componentes/Jogadores/index.js
+++ futebol/src/componentes/Jogadores/index.js
@@ -1,10 +1,20 @@
 import "./Jogadores.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFutbol } from "@fortawesome/free-regular-svg-icons";
 
 const Jogadores = ({ nome, imagem, origem, corDeFundo }) => {
+  const temImagem = imagem && imagem.trim() !== "";
+
   return (
     <div className="jogador">
       <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-        <img src={imagem} alt={nome} />
+        {temImagem ? (
+          <img src={imagem} alt={nome} />
+        ) : (
+          <div className="icone-wrapper">
+            <FontAwesomeIcon icon={faFutbol} className="icone-futebol" />
+          </div>
+        )}
       </div>
       <div className="rodape">
         <h4>{nome}</h4>
